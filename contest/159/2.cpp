aa17a9d7e93198a6abf7e37a79376c818961828d
kavin23923 <kavin23923@gmail.com>
2019-11-03 12:03:35 +0800

    fix coding style

--- contest/159/2.cpp
+++ contest/159/2.cpp
@@ -5,7 +5,7 @@ public:
     int now = 0, next = 1;
     vector<string> res{folder[0]};
     while (newt < folder.size()) {
-      if (!substring(folder[now], folder[next])) {
+      if (!subString(folder[now], folder[next])) {
         res.push_back(folder[next]);
         now = next;
       }
@@ -14,7 +14,7 @@ public:
     return res;
   }
 
-  bool substring (string target, string input) {
+  bool subString (string target, string input) {
     for (int i = 0; i < target.size(); i++) {
       if (i > input.length() || target[i] != input[i]) {
         return false;
