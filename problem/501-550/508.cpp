68ce828defa4b4ed9033b0f2f20e983cdc7eb2d3
chenghsun.yang <chenghsun.yang@shopee.com>
2020-01-24 12:29:02 +0800

    fix coding style

--- problem/501-550/508.cpp
+++ problem/501-550/508.cpp
@@ -9,26 +9,26 @@
  */
 class Solution {
 public:
-	vector<int> findFrequentTreeSum(TreeNode* root) {
-		vector<int> res;
-		unordered_map<int, int> count;
-		int freq = 0;
-		traverse(root, count, &freq);		
+  vector<int> findFrequentTreeSum(TreeNode* root) {
+    vector<int> res;
+    unordered_map<int, int> count;
+    int freq = 0;
+    traverse(root, count, &freq);		
     for (auto it = count.begin(); it != count.end(); it++) {
       if (it->second == freq) {
-        res.push_back(it->first);
+			  res.push_back(it->first);
       }
     }
-		return res;
+    return res;
   }
 
 	int traverse(TreeNode* root, unordered_map<int, int> &count, int *freq) {
-		if (!root) return 0;
-		int l = traverse(root->left, count, freq);
-		int r = traverse(root->right, count, freq);
-		int sum = l + r + root->val;
-		count[sum]++;
-		*freq = max(count[sum], *freq);
-		return sum;
-	}
+    if (!root) return 0;
+    int l = traverse(root->left, count, freq);
+    int r = traverse(root->right, count, freq);
+    int sum = l + r + root->val;
+    count[sum]++;
+    *freq = max(count[sum], *freq);
+    return sum;
+  }
 };
